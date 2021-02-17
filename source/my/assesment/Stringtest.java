1dc2d11ee6bebdce49a6e24ff90694e808ecf7ca
Soumyakhandai <soumya21.khandai@gmail.com>
2021-02-17 16:20:00 -0600

    StringTest

---
+++ source/my/assesment/Stringtest.java
@@ -0,0 +1,16 @@
+package my.assesment;
+
+public class Stringtest {
+	public static void main(String[]args){
+		String mystr="Hello world";
+		String Fname="Soumya";
+		String Lname="Khandai";
+		String demo="Hello";
+		char result=mystr.charAt(7);
+		System.out.println(result);
+		System.out.println(Fname . concat ( Lname ));
+		System.out.println(demo.length());
+		
+	}
+
+}
